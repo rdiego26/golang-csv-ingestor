3314ad2abc5d98b309fb96dbc30a7d8dc72bb2e0
ramos <rdiego26@gmail.com>
2024-01-21 19:55:27 +0000

    test: add test for parse milliseconds

---
+++ csv-processor_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func TestParseMilliseconds(t *testing.T) {
+	input := "1527867048000"
+	want := 1527867048000
+
+	got, err := parseMilliseconds(input)
+	if err != nil {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+
+	wrongInput := "A"
+	result, err := parseMilliseconds(wrongInput)
+	if result != 0 {
+		t.Errorf("result %q, wanted %q", result, 0)
+	}
+
+	if err == nil {
+		t.Errorf("expects err non nil but received %v", err)
+	}
+}
