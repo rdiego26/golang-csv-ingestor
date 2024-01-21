902ac821b9f6b6797ce71e856eb87ceeb5f74f9e
ramos <rdiego26@gmail.com>
2024-01-21 19:39:58 +0000

    test: add test for filterParams

--- db_test.go
+++ db_test.go
@@ -1,1 +1,17 @@
 package main
+
+import "testing"
+
+func TestFilterParams(t *testing.T) {
+	input := make(map[string]string)
+	input["unknown"] = "unknown"
+	input["email"] = "email"
+
+	want := make(map[string]string)
+	want["email"] = "email"
+
+	got := filterParams(input)
+	if len(got) == len(input) {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
